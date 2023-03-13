0d977a0891c36f89b7789ac0df3a01fb4445b841
Marc Fabre <nds.kram@gmail.com>
2023-03-13 16:16:42 +0100

    Corrected condition

--- src/core/database.core.ts
+++ src/core/database.core.ts
@@ -95,9 +95,8 @@ export default class PostgreSQL extends Sequelize {
      * @memberof PostgreSQL
      */
     public async importConfig() {
-        if (process.env.FORK_ID === '0') return
+        if (process.env.FORK_ID !== '0') return
         console.log('IMPORT CONFIG')
-        console.log(process.env.FORK_ID)
         if (!fs.existsSync('./weweb-server.config.json')) return
 
         const config = JSON.parse(fs.readFileSync('./weweb-server.config.json', 'utf8'))
