9aa2525d39f6c37575809be1369e323ced4c1c0e
Marc Fabre <marc@weweb.io>
2024-05-16 12:00:30 +0200

    fix/open-id-token-length (#16)

--- src/controllers/pluginSettings.controller.ts
+++ src/controllers/pluginSettings.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { db } from '../core'
 import { utils, log } from '../services'
+import { RequestWebsite } from 'ww-request'
 const wwmt = require('weweb-microservice-token')
 
 /**
@@ -34,22 +35,34 @@ export const createPluginSettings = async (req: Request, res: Response, next: Ne
  * @param req Request
  * @param res Response
  */
-export const updateAuth0CurrentUser = async (req: Request, res: Response, next: NextFunction) => {
+export const updateAuth0CurrentUser = async (req: RequestWebsite, res: Response, next: NextFunction) => {
     try {
         log.debug('controllers:pluginSettings:updateAuth0CurrentUser')
 
         if (!process.env.WEWEB_PLUGINS_URL) return res.status(403).send('FEATURE_NOT_AVAILABLE')
 
-        if (!utils.isDefined([req.params.designId, req.params.settingsId])) return res.status(400).send({ success: false, code: 'BAD_PARAMS' })
+        if (!utils.isDefined([req.designVersion.designId, req.params.settingsId])) return res.status(400).send({ success: false, code: 'BAD_PARAMS' })
 
-        const settings = await db.models.pluginSettings.findByPk(req.params.settingsId)
+        if (req.headers['x-weweb-cookies']) {
+            try {
+                const cookies = JSON.parse(`${req.headers['x-weweb-cookies']}`)
+                req.cookies.session = cookies.session
+            } catch (err) {
+                log.error(err)
+            }
+        }
+        if (!req.cookies.session) return res.status(401).send('UNAUTHORIZED')
+
+        const settings = await db.models.pluginSettings.findOne({
+            where: { designVersionId: req.designVersion.id, pluginId: 'e93a2dfd-9b19-473e-b445-c666fed4e14a' },
+        })
         if (!settings) return res.status(404).send({ success: false, code: 'NOT_FOUND' })
 
         const { body: data } = await wwmt.post(
-            `${process.env.WEWEB_PLUGINS_URL}/microservice/designs/${req.params.designId}/settings/${settings.id}/auth0/users/current`,
+            `${process.env.WEWEB_PLUGINS_URL}/microservice/designs/${req.designVersion.designId}/settings/${settings.id}/auth0/users/current`,
             { settings, ...req.body },
             null,
-            { session: req.cookies.session }
+            { Cookie: 'session=' + req.cookies.session }
         )
 
         return res.status(200).set({ 'cache-control': 'no-cache' }).send({ success: true, data })
